d53232ddb128a4e404b95493302f5a4c308698e0
Jukki <judejoki@gmail.com>
2015-06-08 19:12:42 +0300

    Set up initial work

---
+++ Assets/camerafollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerafollow : MonoBehaviour {
+
+	public GameObject ball;
+	Vector3 cameradistance = new Vector3 (0.0F, 10.0F, 0.0F);
+
+
+	void Update () {
+			transform.position = ball.transform.position + cameradistance;
+	}
+}
