d0357a1cf822fdb86fb97af4efce331a98d3e8dd
Jukki <judejoki@gmail.com>
2015-06-10 23:25:13 +0300

    end of day 3

---
+++ Assets/TileTexture.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<SpriteRenderer>().material.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y);
+	}
+}
