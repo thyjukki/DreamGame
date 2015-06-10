d939b84bcd25b8d4a1c8a4794da9e68a0e3b9946
Jukki <judejoki@gmail.com>
2015-06-10 18:05:30 +0300

    New movement script

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,27 +6,33 @@ public class movement : MonoBehaviour {
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
-	private Vector3 moveDirection = Vector3.zero;
-
+	private Vector2 moveDirection = Vector2.zero;
+	
 	void Update() {
-		Rigidbody2D controller = GetComponent<Rigidbody2D>();
+		CharacterController controller = GetComponent<CharacterController>();
 
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical"));
-                      
+		// Init intial move vector
+		moveDirection = new Vector2(Input.GetAxis("Horizontal"), 0.0F);
+
+		
 		//moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection = moveDirection.normalized;
 		//print (moveDirection.magnitude);
-
+		
 		moveDirection *= speed * Time.deltaTime;
+		
 
+		//check if we are grounded so we can jump
+		if (controller.isGrounded) {
+			if (Input.GetKeyDown("space"))
+				moveDirection += new Vector2(0,jumpSpeed);
+		}
 		/*if (Input.GetButton("Jump"))
 			moveDirection.y = jumpSpeed;
 		*/
-		if(moveDirection.magnitude != 0)
-			transform.LookAt(transform.position + moveDirection);
+
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
-
+		
 	}
 }
-
