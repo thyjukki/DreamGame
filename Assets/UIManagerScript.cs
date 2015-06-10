cf43fddabc328328bb73b81883a4fe6efbc1d382
Lawri <lauri.s.lapplainen@gmail.com>
2015-06-10 16:01:34 +0300

    Settings menu

--- Assets/UIManagerScript.cs
+++ Assets/UIManagerScript.cs
@@ -5,11 +5,21 @@ public class UIManagerScript : MonoBehaviour {
 	
 	public Animator startButton;
 	public Animator settingsButton;
+	public Animator dialog;
 
 	public void OpenSettings()
 	{
 		startButton.SetBool("isHidden", true);
 		settingsButton.SetBool("isHidden", true);
+		dialog.enabled = true;
+		dialog.SetBool("isHidden", false);
+	}
+
+	public void CloseSettings()
+	{
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
 	}
 
 	public void StartGame()
