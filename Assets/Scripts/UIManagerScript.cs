cab581fe334aef5ed3ffe240af0c5260a1e596c9
Lawri <lauri.s.lapplainen@gmail.com>
2015-06-10 19:05:22 +0300

    Sliding menu animated

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,6 +6,23 @@ public class UIManagerScript : MonoBehaviour {
 	public Animator startButton;
 	public Animator settingsButton;
 	public Animator dialog;
+	public Animator contentPanel;
+
+	public void ToggleMenu()
+	{
+		contentPanel.enabled = true;
+		
+		bool isHidden = contentPanel.GetBool("isHidden");
+		contentPanel.SetBool("isHidden", !isHidden);
+	}
+
+	void Start()
+	{
+		RectTransform transform = contentPanel.gameObject.transform as RectTransform;        
+		Vector2 position = transform.anchoredPosition;
+		position.y -= transform.rect.height;
+		transform.anchoredPosition = position;
+	}
 
 	public void OpenSettings()
 	{
