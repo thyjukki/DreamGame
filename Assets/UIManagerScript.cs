645bb1647b293e4229b3dd0a7a93ec90196a16bd
Lawri <lauri.s.lapplainen@gmail.com>
2015-06-10 15:29:23 +0300

    Animation controller

--- Assets/UIManagerScript.cs
+++ Assets/UIManagerScript.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
+	
+	public Animator startButton;
+	public Animator settingsButton;
+
+	public void OpenSettings()
+	{
+		startButton.SetBool("isHidden", true);
+		settingsButton.SetBool("isHidden", true);
+	}
 
 	public void StartGame()
 	{
