c9a8620169b295cf5c2f5bcec6900611eee30b0e
Jukki <judejoki@gmail.com>
2015-06-11 22:42:51 +0300

    added flame object and sprites

---
+++ Assets/inventory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inventory : MonoBehaviour {
+
+	public GameObject[] items; //items in inventory
+	public Vector3[] inventoryPositions; //positions of inventory slots
+
+	void Start () {
+	}
+
+	void Update () {
+		for (int i = 0; i < items.Length; i++) {
+			Instantiate (items [i], inventoryPositions [i], Quaternion.identity);
+		}
+	}
+}
