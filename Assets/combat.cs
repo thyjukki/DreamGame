2dd34c4dcc560514b0be12f74b8612b5fd4332d0
Jukki <judejoki@gmail.com>
2015-06-12 15:32:46 +0300

    combat trigger works now

--- Assets/combat.cs
+++ Assets/combat.cs
@@ -4,32 +4,28 @@ using System.Collections.Generic;
 
 public class combat : MonoBehaviour {
 
-	private List<Collider2D> enemiesInTriggerArea = new List<Collider2D>();
-
+	/*private List<Collider2D> enemiesInTriggerArea = new List<Collider2D>();
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Enemy") {
-			if (!enemiesInTriggerArea.Contains (other))
-				enemiesInTriggerArea.Add (other);
+
+			enemiesInTriggerArea.Add (other);
 		}
 			
 	}
 
 	void OnTriggerLeave2D(Collider2D other){
 		if (other.gameObject.tag == "Enemy") {
-			if (enemiesInTriggerArea.Contains (other))
+
 				enemiesInTriggerArea.Remove (other);
 		}
 
-	}
+	} TODO: multiple enemies hit? */
 
-	void Update (){
-		if (Input.GetButtonDown ("Hit")) {
-			for (int i = 1; i < enemiesInTriggerArea.Count; i++) {
-				print(enemiesInTriggerArea[i].gameObject);
-				print ("anus");
-			}
-			print ("gay");
+	void OnTriggerStay2D (Collider2D other){
+		if (other.gameObject.tag == "Enemy" && Input.GetButtonDown ("Hit")) {
+			print (other.gameObject);
 		}
 	}
+
 
 }
