1b3d0ecce9c8665ea5b25aa93119cbb766dcfaa1
Jukki <judejoki@gmail.com>
2015-06-12 21:20:16 +0300

    Added doors that can lead to other levels

---
+++ Assets/DoorPortal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPortal : MonoBehaviour {
+
+	public string levelToLoad;
+	GameObject player;
+
+	void Awake (){
+		player = GameObject.Find ("Hobo");
+		player.GetComponent<BoxCollider2D> ();
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+
+		if ((other.GetComponent<BoxCollider2D> () == player.GetComponent<BoxCollider2D> ()) && (Input.GetAxis ("Vertical") > 0.01))
+			Application.LoadLevel (levelToLoad);
+
+	}
+}
