fe3acbf5f0302279f338044ad8ba6cd6ad36a847
Jukki <judejoki@gmail.com>
2015-06-12 18:57:49 +0300

    projectiles deal damage now

--- Assets/DestroyOnCollision.cs
+++ Assets/DestroyOnCollision.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class DestroyOnCollision : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag != "Player" && other.gameObject.tag != "Check") {
+
+		if (other.gameObject.tag == "Enemy") {
+			Health script = (Health)other.gameObject.GetComponent (typeof(Health));
+			script.Damage (5);
+			Destroy (gameObject);
+		}else if (other.gameObject.tag != "Player" && other.gameObject.tag != "Check") {
 			Destroy (gameObject);
 		}
+
+
 	}
 }
