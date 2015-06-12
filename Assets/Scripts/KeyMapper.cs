c59c79b4ddd9a1cd04ecfd3b31a37381f13f1580
Lawri <lauri.s.lapplainen@gmail.com>
2015-06-12 19:22:49 +0300

    Fixed bug with control buttons
    Keybinding script done

--- Assets/Scripts/KeyMapper.cs
+++ Assets/Scripts/KeyMapper.cs
@@ -39,8 +39,22 @@ public class KeyMapper : MonoBehaviour {
 
 				if (change && e.isKey)
 				{
-					inputManager[key].key = e.keyCode.ToString();
-					PlayerPrefs.SetString(inputManager[key].keyName, e.keyCode.ToString());
+					//Fix the control/ctrl issue
+					if (e.keyCode.ToString () == "LeftControl")
+					{
+						inputManager[key].key = "left ctrl";
+						PlayerPrefs.SetString (inputManager[key].keyName, "left ctrl");
+					}
+					else if (e.keyCode.ToString() == "RightControl")
+					{
+						inputManager[key].key = "right ctrl";
+						PlayerPrefs.SetString (inputManager[key].keyName, "right ctrl");
+					}
+					else
+					{
+						inputManager[key].key = e.keyCode.ToString();
+						PlayerPrefs.SetString(inputManager[key].keyName, e.keyCode.ToString());
+					}
 					change = false;
 				}
 			}
@@ -64,13 +78,18 @@ public class KeyMapper : MonoBehaviour {
 		{
 			InputManager shoot = inputManager.Find(str => string.Equals(str.keyName, "Shoot"));
 
-			if(Input.GetKeyDown (shoot.key))
+			if(Input.GetKeyDown (shoot.key.ToLower ()))
 				print (shoot.keyName);
 
 			InputManager jump = inputManager.Find (str => string.Equals(str.keyName, "Jump"));
 
-			if(Input.GetKeyDown(jump.key))
+			if(Input.GetKeyDown(jump.key.ToLower()))
 				print (jump.keyName);
+
+			InputManager crouch = inputManager.Find (str => string.Equals(str.keyName, "Crouch"));
+			
+			if(Input.GetKeyDown(crouch.key.ToLower()))
+				print (crouch.keyName);
 		}
 	}
 }
