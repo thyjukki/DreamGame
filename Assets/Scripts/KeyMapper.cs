94ade8f31e75d63bb22f77bf3fe78d18abf2db48
Lawri <lauri.s.lapplainen@gmail.com>
2015-06-12 18:45:43 +0300

    Key mapper script

---
+++ Assets/Scripts/KeyMapper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyMapper : MonoBehaviour {
+
+	public List<InputManager> inputManager;
+
+	bool change;
+	int key;
+
+	[System.Serializable]
+	public class InputManager
+	{
+		public string keyName;
+		public string key;
+	}
+
+	void OnGUI()
+	{
+		Event e = Event.current;
+
+		if (inputManager != null)
+		{
+			for (int i = 0; i < inputManager.Count; i++)
+			{
+				GUILayout.BeginHorizontal();
+
+				GUILayout.Label(inputManager[i].keyName);
+
+				if (GUILayout.Button(inputManager[i].key))
+				{
+					key = i;
+					change = true;
+				}
+
+				GUILayout.EndHorizontal();
+
+				if (change && e.isKey)
+				{
+					inputManager[key].key = e.keyCode.ToString();
+					change = false;
+				}
+			}
+		}
+	}
+}
