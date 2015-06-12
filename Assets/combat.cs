dd988e5f8c012b6a4fa14d12c0c0a7136a506167
Jukki <judejoki@gmail.com>
2015-06-12 15:23:24 +0300

    combat

--- Assets/combat.cs
+++ Assets/combat.cs
@@ -4,25 +4,31 @@ using System.Collections.Generic;
 
 public class combat : MonoBehaviour {
 
-	private List<GameObject> enemiesInTriggerArea = new List<GameObject>();
+	private List<Collider2D> enemiesInTriggerArea = new List<Collider2D>();
 
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.tag == "Enemy")
-			enemiesInTriggerArea.Add (other.gameObject);
+		if (other.gameObject.tag == "Enemy") {
+			if (!enemiesInTriggerArea.Contains (other))
+				enemiesInTriggerArea.Add (other);
+		}
 			
 	}
 
 	void OnTriggerLeave2D(Collider2D other){
-		if (other.tag == "Enemy")
-			enemiesInTriggerArea.Remove (other.gameObject);
+		if (other.gameObject.tag == "Enemy") {
+			if (enemiesInTriggerArea.Contains (other))
+				enemiesInTriggerArea.Remove (other);
+		}
 
 	}
 
 	void Update (){
 		if (Input.GetButtonDown ("Hit")) {
 			for (int i = 1; i < enemiesInTriggerArea.Count; i++) {
-				print(enemiesInTriggerArea[i]);
+				print(enemiesInTriggerArea[i].gameObject);
+				print ("anus");
 			}
+			print ("gay");
 		}
 	}
 
