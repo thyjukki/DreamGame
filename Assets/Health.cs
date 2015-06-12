52b45cc75fed435a2a3e94b0c12e9b900d9a0862
thyjukki <judejoki@gmail.com>
2015-06-12 15:23:52 +0300

    Added playrKilled method

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class Health : MonoBehaviour {
+	public float maxHealth = 5;
+	public float curHealth = 5;
+	private bool playerDead = false;
 
-	// Use this for initialization
-	void Start () {
-	
+
+	void Start()
+	{
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Damage(float dmg)
+	{
+		if (playerDead)
+			return;
+		curHealth -= dmg;
+
+		if (curHealth <= 0)
+			playerKilled();
 	}
+
+	void playerKilled()
+	{
+		playerDead = true;
+
+		print ("Player Died");
+	}
+
 }
