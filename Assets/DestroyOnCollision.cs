898efffbea62907801b9da3911eba198fafeed2c
Jukki <judejoki@gmail.com>
2015-06-12 18:36:52 +0300

    shooting/throwing works now, fixed ammo hitbox

--- Assets/DestroyOnCollision.cs
+++ Assets/DestroyOnCollision.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class DestroyOnCollision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag != "Player" && other.gameObject.tag != "Check") {
+			Destroy (gameObject);
+		}
 	}
 }
