47b5d30d2d872579506944b97a359bd97b8efca7
Oskiii <oliukku@gmail.com>
2015-06-14 00:27:55 +0300

    quick and (very dirty) fix to inventory flipping when hobo does

--- Assets/inventory.cs
+++ Assets/inventory.cs
@@ -11,6 +11,7 @@ public class inventory : MonoBehaviour {
 	private List<GameObject> instantiatedItems = new List<GameObject>();
 
 	void Start () {
+
 	}
 
 	void Update () {
@@ -25,18 +26,31 @@ public class inventory : MonoBehaviour {
 			for (int i = 0; i < items.Count; i++) { //go through each item in inventory
 				//and instantiate them inventory positions
 				GameObject item = (GameObject) Instantiate (items [i], inventoryPositions [i].transform.position, Quaternion.identity);
-				//set item as child of Hobo so they follow him
-				item.transform.parent = transform;
-				//scale item to half size
+
+				//scale item to itemSize
 				item.transform.localScale = new Vector3(itemSize, itemSize, item.transform.localScale.z);
 				//set item's sorting layer to UI so it renders in front
 				item.GetComponent<SpriteRenderer>().sortingLayerName = "UI";
+
+				//set colliders off
+				if (item.GetComponent<BoxCollider2D> () != null)
+					item.GetComponent<BoxCollider2D> ().enabled = false;
+				if (item.GetComponent<CircleCollider2D> () != null)
+					item.GetComponent<CircleCollider2D> ().enabled = false;
+
 				//add item to list of instantiated items
 				instantiatedItems.Add(item);
 				
 			}
 			
 		}
+
+		if (inventoryOpen) {
+			for (int i = 0; i < instantiatedItems.Count; i++) {
+				instantiatedItems[i].transform.position = inventoryPositions[i].transform.position;
+			}
+		}
+
 	}
 
 	void EmptyList (){
