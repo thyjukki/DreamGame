22a25f9a7be4426ad057af7a80761b37217b24e5
Oskiii <oliukku@gmail.com>
2015-06-15 23:25:18 +0300

    shadow monsters

--- Assets/shadowMonster.cs
+++ Assets/shadowMonster.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class shadowMonster : MonoBehaviour {
 
 	void Start () {
-		enabled = false;
+		gameObject.GetComponent<SpriteRenderer>().enabled = false;
+		gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+		gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
 	}
 
 	void Update () {
-		if (HoboController.dreaming = true) {
-			enabled = true;
+		if (HoboController.dreaming == true) {
+			gameObject.GetComponent<SpriteRenderer>().enabled = true;
+			gameObject.GetComponent<BoxCollider2D> ().enabled = true;
+			gameObject.GetComponent<Rigidbody2D> ().gravityScale = 1;
 		} else {
-			enabled = false;
+			gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+			gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
 		}
 	}
 }
