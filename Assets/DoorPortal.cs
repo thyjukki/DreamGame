ac0326de3523bdd2a49ae4c1268476341eb77426
Oskiii <oliukku@gmail.com>
2015-06-15 15:43:48 +0300

    door now only accepts player

--- Assets/DoorPortal.cs
+++ Assets/DoorPortal.cs
@@ -6,14 +6,9 @@ public class DoorPortal : MonoBehaviour {
 	public string levelToLoad;
 	GameObject player;
 
-	void Awake (){
-		player = GameObject.Find ("Hobo");
-		player.GetComponent<BoxCollider2D> ();
-	}
-
 	void OnTriggerStay2D(Collider2D other){
 
-		if ((other.GetComponent<BoxCollider2D> () == player.GetComponent<BoxCollider2D> ()) && (Input.GetAxis ("Vertical") > 0.01))
+		if ((other.name == "Hobo") && (Input.GetAxis ("Vertical") > 0.01))
 			Application.LoadLevel (levelToLoad);
 
 	}
