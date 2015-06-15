886da42d5ee401330ee9cfbde50870d3a27ba322
Oskiii <oliukku@gmail.com>
2015-06-15 18:57:41 +0300

    clouds move

---
+++ Assets/cloudMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloudMove : MonoBehaviour {
+
+	public float cloudSpeed;
+	public float startx;
+	public float endx;
+
+
+	void Awake (){
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (cloudSpeed, 0f);
+
+	}
+
+	void Update (){
+		if (gameObject.transform.position.x > endx)
+			gameObject.transform.position = new Vector2(startx, gameObject.transform.position.y);
+	}
+}
